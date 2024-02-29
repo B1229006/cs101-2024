2594036d6151090c8315379c065ebf53fbddcf0d
B1229006 <144314502+B1229006@users.noreply.github.com>
2024-02-29 14:25:46 +0800

    Update main.c

--- EX1/main.c
+++ EX1/main.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+//using namespace std;
+void print_sp(int rows,int n){
+for (int sp=0;sp<rows-n;sp++){
+		printf(" ");
+}}
+void print_num(int n){
+	for (int i=0;i<n;i++){
+	    printf("%d",n);
+	    printf(" ");
+	
+	}
+}
+int main (){
+	int rows=6;
+	for (int n=0;n<=rows;n++){
+		print_sp(rows,n);
+		print_num(n);
+		printf("\n");
+	}
+}
