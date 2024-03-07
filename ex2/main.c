c57584aac389da1d1d14125c1b4792befe30941c
B1229006 <144314502+B1229006@users.noreply.github.com>
2024-03-07 15:46:35 +0800

    Update main.c

--- ex2/main.c
+++ ex2/main.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+
+int main() {
+    int a[] = {0, 1, 2};
+    char b[] = "ABC";
+    float c[] = {1.1, 1.2, 1.3};
+    FILE *file;
+
+    file = fopen("a.bin", "wb+");
+    if (file == NULL) {
+        printf("無法開啟檔案\n");
+        return 1;
+    }
+
+    
+    fwrite(a, sizeof(int), sizeof(a)/sizeof(int), file);
+    fwrite(b, sizeof(char), sizeof(b), file);
+    fwrite(c, sizeof(float), sizeof(c)/sizeof(float), file);
+
+    
+    fclose(file);
+
+    file = fopen("a.bin", "rb");
+    if (file == NULL) {
+        printf("無法開啟檔案。\n");
+        return 1;
+    }
+
+    
+    int readInt[3];
+    char readChar[4];
+    float readFloat[3];
+
+
+    fread(readInt, sizeof(int), 3, file);
+    fread(readChar, sizeof(char), 4, file);
+    fread(readFloat, sizeof(float), 3, file);
+    fclose(file);
+
+    
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", readInt[i]);
+    }
+    printf("\n");
+
+   
+    printf("%s\n", readChar);
+
+    
+    for (int i = 0; i < 3; i++) {
+        printf("%f ", readFloat[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
 
