560b2834d7c8a044b7ba33b5a8b7339b5d17c6f8
B1229006 <144314502+B1229006@users.noreply.github.com>
2024-05-23 13:59:43 +0800

    Update main.cpp

--- EX5/main.cpp
+++ EX5/main.cpp
@@ -1,1 +1,34 @@
+#include <bits/stdc++.h>                                                                                                                                                                
+using namespace std;
 
+class myString {
+	private:
+		string m_str;
+	public:
+		myString(string s){
+			m_str=s;
+		}
+}; 
+
+class ReadClass{
+	ifstream in;
+	string line;
+	in.open("main.cpp");
+	if (in.fail()){
+		cout <<"Error opening a file"<<endl;
+		in.close();
+	}
+	while (getline(in,line)){
+		cout <<line<<endl;
+	}
+	                                                                                                                                                                                                                                                                                        
+	
+};
+
+int main (){
+	
+	ReadClass rfile;
+	rfile.showClass();
+	return 0;
+}
+                                                                                                       
